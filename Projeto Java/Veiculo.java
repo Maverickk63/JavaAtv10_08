5337744796df51361c2ac8d3f83bf93da2335eb2
Maverickk63 <103067766+Maverickk63@users.noreply.github.com>
2023-08-10 08:49:50 -0300

    Add files via upload

---
+++ Projeto Java/Veiculo.java
@@ -0,0 +1,9 @@
+public class Veiculo{
+    private int ano;
+    private String marca;
+    private String cor;
+    private String placa;
+    
+    private Modelo modelo;
+
+}
---
+++ Projeto Java/seguro.java
@@ -0,0 +1,8 @@
+public class Seguro{
+    private float valor;
+    private boolean vigencia;
+
+    private Veiculo veiculo;
+    private Cliente proprietario;
+    private Cliente[] assegurados;
+}
